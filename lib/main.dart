c89a0018a2fe3aecb2e9363385b081e80526d31e
Maricor <m.masong14@gmail.com>
2023-09-09 14:14:00 +0800

    create a BigCard widget with own style

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
         home: const HomePage(),
