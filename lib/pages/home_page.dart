6b70ce491059d404d29a91c52ec6b18e8eab7b9c
Maricor <m.masong14@gmail.com>
2023-09-09 22:45:55 +0800

    refractor generator page to its own file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../states/my_app_state.dart';
 import 'generator_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,23 +7,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var wordPair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(wordPair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return SafeArea(
-      child: Scaffold(
-        body: GeneratorPage(
-          wordPair: wordPair, 
-          appState: appState, 
-          icon: icon),
-      ),
+    return const SafeArea(
+      child: GeneratorPage(),
     );
   }
 }
